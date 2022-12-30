366084511bd49a8450da2f5af2d6227cbea0b8f4
Syncronomi <raphaelaguirre2405@gmail.com>
2022-12-30 23:19:25 +0800

    update

--- function.js
+++ function.js
@@ -1,4 +1,29 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+/*Navbar*/
+function myfunction(){
+  var x =
+  document.getElementById("mylinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+/*Dark-Mode*/
+function myFunction(){
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+/*Projects*/
+function projects(evt, Name) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Name).style.display = "block";
+  evt.currentTarget.className += " active";
+}
